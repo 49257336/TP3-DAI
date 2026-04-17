5bf5cfe126c57854b935b7d023a12392fd78ed5a
MILENA WINER <49257336@ort>
2026-04-17 11:15:45 -0300

    Se arreglo el parseCSV.js para que genere un archivo correctamente

--- parseCSV.js
+++ parseCSV.js
@@ -3,9 +3,11 @@ import fs from 'fs';
 function parsearArchivo(){
     const data = fs.readFileSync('productos.json', 'utf-8');
     const productos = JSON.parse(data);
+    let contenido = 'nombre,precio\n';
     productos.forEach(producto => {
-        fs.writeFileSync(`${producto.nombre}, ${producto.precio}`);
+        contenido += `${producto.nombre},${producto.precio}\n`;
     });
-}
 
+    fs.writeFileSync('productos.csv', contenido);
+}
 export default parsearArchivo;
