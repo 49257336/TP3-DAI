3ba48677e418d77c40829d8ca93bc7b0d95cc475
MILENA WINER <49257336@ort>
2026-04-20 07:59:25 -0300

    Se arregló la función validarPassword

--- validarPassword.js
+++ validarPassword.js
@@ -2,20 +2,20 @@ function validarPassword(password){
 var esValida = false;
 var hasN = false;
 var hasU = false;
-password.split().forEach(element => {
-        if(!Number.isNaN(element)){
+password.split('').forEach(element => {
+        if(!isNaN(element)){
             hasN = true;
         }
         if(element == element.toUpperCase()){
             hasU = true;
         }
     });
-if(password.lenght >= 8 && hasN && hasU){
+if(password.length >= 8 && hasN && hasU){
     esValida = true;
 }
 
 
-if(esValida){
+if(esValida==true){
     console.log("La constraseña es valida");
 }else{
     console.log("La constraseña NO es valida");
